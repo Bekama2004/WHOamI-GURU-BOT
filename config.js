b667047008cdd076e4b1eee3d34e93bba1d1cf39
Bekama2004 <120168969+Bekama2004@users.noreply.github.com>
2023-05-16 20:16:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94775434030', '𝑀𝓇. 𝐻𝑒𝓈𝒽𝒶𝓃', true],
+  ['14245296612', 'Jasmine ✨️', true], 
   [''] 
 ] //Number of owners
 
@@ -47,7 +47,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'SANDARU-YT'
+global.botname = 'Jasmine ✨️'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
